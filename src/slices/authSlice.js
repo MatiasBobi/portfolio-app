761d50329e6f0ad80d47b9c9d8401488cd96fbd8
MatiasBobi <matiasleonardobobi@gmail.com>
2023-10-05 22:11:24 -0300

    delete console logs

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,17 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
-
 const initialState ={
     userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem("userInfo")) : null,
     token: localStorage.getItem("user_token") ? JSON.parse(localStorage.getItem("user_token")) : null
 }
 
-/*
-{
-    userInfo: localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem("userInfo")) : null,
-}
-*/
 const authSlice = createSlice({
     name: "auth",
     initialState,
@@ -20,7 +13,6 @@ const authSlice = createSlice({
             const {userInfo, token} = action.payload
             state.userInfo = userInfo
             state.token = token
-            console.log(action.payload)
             localStorage.setItem("userInfo", JSON.stringify(action.payload.userInfo))
             localStorage.setItem("user_token", JSON.stringify(action.payload.token))
         },
